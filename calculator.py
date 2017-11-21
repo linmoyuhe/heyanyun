047d0fa0319e1c6f2a8134e2ce8649260b52807b
heyanyun0306 <heyanyun0306@163.com>
2017-11-21 20:52:32 +0800

    renew a file

--- calculator.py
+++ calculator.py
@@ -3,14 +3,19 @@ import sys
 if __name__ == '__main__':
    if len(sys.argv) != 1:
      for arg in sys.argv[1:]:
-       try:
-         argv_str = arg.split(':')
-         ID = argv_str[0]
-         salary = argv_str[1]
-         try:
-            int_salary = int(salary)
+            try:
+                argv_str = arg.split(':')
+            except:
+                print("Parameter Error")
+            ID = argv_str[0]
+            salary = argv_str[1]
+            try:
+               int_salary = int(salary)
+            except:
+               print("Parameter Error")
             social_security_fee = int_salary * 16.5 / 100
             tax_salary = int_salary - social_security_fee - 3500
+     
             if tax_salary <= 1500:
                tax_rate, quick_deduction = 0.03, 0
             elif tax_salary <= 4500:
@@ -26,12 +31,10 @@ if __name__ == '__main__':
             else: 
                tax_rate, quick_deduction = 0.45, 13505
             tax_payable = tax_salary * tax_rate - quick_deduction
+            if tax_payable <= 0:
+               tax_payable = 0
             after_tax_salary = int_salary - social_security_fee - tax_payable
-            print(ID + ':' + format(after_tax_salary, '.2f'))
-         except:
-            print("Parameter Error")
-       except:
-          print("Parameter Error")  
+            print(ID + ':' + format(after_tax_salary, '.2f'))  
    else:
        print("Parameter Error")
    sys.exit(0)
